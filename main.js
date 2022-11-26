cfa761dcc07c5490cf710ac56457bf78abc1299a
Harshit Paneri <harshitpanari@gmail.com>
2022-11-27 01:10:21 +0530

    merge

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 3000
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+const { mergePdfs } = require("./merge");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,"template/index.html"))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "template/index.html"));
+});
 
-app.post('/merge', upload.array('pdfs', 2), function (req, res, next) {
-    console.log(req.files)
-    res.send({data : req.files})
-    // req.files is array of `photos` files
-    // req.body will contain the text fields, if there were any
-  })
+app.post("/merge", upload.array("pdfs", 2), async (req, res, next) => {
+  console.log(req.files);
+  await mergePdfs(
+    path.join(__dirname, req.files[0].path),
+    path.join(__dirname, req.files[1].path)
+  );
+  res.redirect("http://localhost:3000/");
+  res.send({ data: req.files });
+  // req.files is array of `photos` files
+  // req.body will contain the text fields, if there were any
+});
 
 // app.post('/', (req, res) => {
 //     res.sendFile(path.join(__dirname,"template/index.html"))
 //   })
-  
+
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
