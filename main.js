1b3410b707a9b12cfd4245c83bdb8dca1d272f1e
Harshit Paneri <harshitpanari@gmail.com>
2022-11-26 21:52:07 +0530

    multer added

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const path = require('path')
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const app = express()
 const port = 3000
 
@@ -7,6 +9,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,"template/index.html"))
 })
 
+app.post('/merge', upload.array('[df]', 2), function (req, res, next) {
+    // req.files is array of `photos` files
+    // req.body will contain the text fields, if there were any
+  })
+
+app.post('/', (req, res) => {
+    res.sendFile(path.join(__dirname,"template/index.html"))
+  })
+  
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
