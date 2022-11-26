856d187a0d6a182272208a93772cd95744d5a566
Harshit Paneri <harshitpanari@gmail.com>
2022-11-26 22:22:27 +0530

    error solve

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 const express = require('express')
 const path = require('path')
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
 const app = express()
 const port = 3000
+
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,"template/index.html"))
 })
 
-app.post('/merge', upload.array('[df]', 2), function (req, res, next) {
+app.post('/merge', upload.array('pdfs', 2), function (req, res, next) {
+    console.log(req.files)
+    res.send({data : req.files})
     // req.files is array of `photos` files
     // req.body will contain the text fields, if there were any
   })
 
-app.post('/', (req, res) => {
-    res.sendFile(path.join(__dirname,"template/index.html"))
-  })
+// app.post('/', (req, res) => {
+//     res.sendFile(path.join(__dirname,"template/index.html"))
+//   })
   
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
