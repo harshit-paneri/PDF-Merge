06c24a954cc4bcc749c1a7e129651d6597b8334e
Harshit Paneri <harshitpanari@gmail.com>
2022-11-27 11:02:46 +0530

    merge run

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3000;
-const { mergePdfs } = require("./merge");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const { mergePdfs } = require("./merge");
 
-app.use(express.static("public"));
+const upload = multer({ dest: "uploads/" });
+app.use('/static',express.static("public"));
+const port = 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "template/index.html"));
@@ -14,19 +14,14 @@ app.get("/", (req, res) => {
 
 app.post("/merge", upload.array("pdfs", 2), async (req, res, next) => {
   console.log(req.files);
-  await mergePdfs(
+  let d = await mergePdfs(
     path.join(__dirname, req.files[0].path),
     path.join(__dirname, req.files[1].path)
   );
-  res.redirect("http://localhost:3000/");
+  res.redirect(`http://localhost:3000/static/${d}.pdf`);
   res.send({ data: req.files });
-  // req.files is array of `photos` files
-  // req.body will contain the text fields, if there were any
 });
 
-// app.post('/', (req, res) => {
-//     res.sendFile(path.join(__dirname,"template/index.html"))
-//   })
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
--- merge.js
+++ merge.js
@@ -5,12 +5,10 @@ var merger = new PDFMerger();
 const mergePdfs = async (p1, p2) => {
   await merger.add(p1); //merge all pages. parameter is the path to file and filename.
   await merger.add(p2);
+  let d = new Date().getTime()
 
-  await merger.save("public/merged.pdf"); //save under given name and reset the internal document
+  await merger.save(`public/${d}.pdf`); //save under given name and reset the internal document
 
-  // Export the merged PDF as a nodejs Buffer
-  // const mergedPdfBuffer = await merger.saveAsBuffer();
-  // fs.writeSync('merged.pdf', mergedPdfBuffer);
 };
 
 module.exports = { mergePdfs };
